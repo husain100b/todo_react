710512b8c864d345c2b0794dcdae548882328331
Husain Ahmmed <husainahmmed1b@gmail.com>
2019-05-28 00:01:51 +0600

    update state from form input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {TodoForm, TodoList} from './components/todo';
+import {addTodo, generateId} from './lib/todoHelpers';
 
 class App extends Component {
   constructor() {
@@ -15,6 +16,18 @@ class App extends Component {
       currentTodo: ''
     }
     this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault()
+    const newId = generateId()
+    const newTodo = {id: newId, name: this.state.currentTodo, isComplete: false}
+    const updateTodos = addTodo(this.state.todos, newTodo)
+    this.setState({
+      todos: updateTodos,
+      currentTodo: ''
+    })
   }
 
   handleInputChange (evt) {
@@ -32,7 +45,9 @@ class App extends Component {
         </header>
         <div className="Todo-App">
           <TodoForm handleInputChange={this.handleInputChange}
-            currentTodo={this.state.currentTodo} />
+            currentTodo={this.state.currentTodo} 
+              handleSubmit={this.handleSubmit}
+            />
           <TodoList todos={this.state.todos} />
         </div>
       </div>
--- src/components/todo/TodoForm.js
+++ src/components/todo/TodoForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const TodoForm = (props) => (
-    <form>
+    <form onSubmit={props.handleSubmit}>
         <input 
             type="text"
             onChange={props.handleInputChange} 
@@ -12,5 +12,6 @@ export const TodoForm = (props) => (
 
 TodoForm.propTypes = {
     currentTodo: PropTypes.string.isRequired,
-    handleInputChange: PropTypes.func.isRequired
+    handleInputChange: PropTypes.func.isRequired,
+    handleSubmit: PropTypes.func.isRequired
 }
