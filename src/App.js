4726bcf89f2f1df09a43614f933846724048e5bd
Husain Ahmmed <husainahmmed1b@gmail.com>
2019-05-27 22:46:22 +0600

    Refector react component to be stateless function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {TodoForm, TodoList} from './components/todo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [
+        {id: 1, name: 'Learn JSX', isComplete: true},
+        {id: 2, name: 'Build an Awesome App', isComplete: false},
+        {id: 3, name: 'Ship It!', isComplete: false}
+      ],
+      currentTodo: ''
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
+  handleInputChange (evt) {
+    this.setState({
+      currentTodo: evt.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>React Todos</h2>
+        </header>
+        <div className="Todo-App">
+          <TodoForm handleInputChange={this.handleInputChange}
+            currentTodo={32} />
+          <TodoList todos={this.state.todos} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/todo/TodoForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const TodoForm = (props) => (
+    <form>
+        <input 
+            type="text"
+            onChange={props.handleInputChange} 
+            value={props.currentTodo}/>
+    </form>
+);
+
+TodoForm.propTypes = {
+    currentTodo: PropTypes.string
+}
