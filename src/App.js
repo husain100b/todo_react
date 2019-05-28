92bc8755e69872d2232411babf63b5f3c73accec
Husain Ahmmed <husainahmmed1b@gmail.com>
2019-05-28 11:11:16 +0600

    Prevent empty form values with conditional submit

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
     }
     this.handleInputChange = this.handleInputChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleEmptySubmit = this.handleEmptySubmit.bind(this)
   }
 
   handleSubmit(evt) {
@@ -26,7 +27,15 @@ class App extends Component {
     const updateTodos = addTodo(this.state.todos, newTodo)
     this.setState({
       todos: updateTodos,
-      currentTodo: ''
+      currentTodo: '',
+      errorMessage: ''
+    })
+  }
+
+  handleEmptySubmit(evt) {
+    evt.preventDefault()
+    this.setState({
+      errorMessage: 'Please supply a todo name'
     })
   }
 
@@ -37,6 +46,7 @@ class App extends Component {
   }
 
   render() {
+    const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit
     return (
       <div className="App">
         <header className="App-header">
@@ -44,9 +54,10 @@ class App extends Component {
           <h2>React Todos</h2>
         </header>
         <div className="Todo-App">
+          {this.state.errorMessage && <span className="error">{this.state.errorMessage}</span>}
           <TodoForm handleInputChange={this.handleInputChange}
             currentTodo={this.state.currentTodo} 
-              handleSubmit={this.handleSubmit}
+              handleSubmit={submitHandler}
             />
           <TodoList todos={this.state.todos} />
         </div>
