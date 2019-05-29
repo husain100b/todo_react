da442d5692ee10e02d343130705078fb6e0e4617
Husain Ahmmed <husainahmmed1b@gmail.com>
2019-05-29 11:35:10 +0600

    create a pipe function

--- src/lib/utils.test.js
+++ src/lib/utils.test.js
@@ -1,4 +1,4 @@
-import {partial} from './utils'
+import {partial, pipe} from './utils'
 
 const add = (a, b) => a + b
 const addThree = (a, b, c) => a + b + c
@@ -13,4 +13,22 @@ test('partial applies the multiple arguments ahead of time', () => {
     const inc = partial(addThree, 1, 3)
     const result = inc(2)
     expect(result).toBe(6)
+})
+
+test('pipe passes the results of inc to dbl', () => {
+    const pipeline = pipe(inc, dbl)
+    const result = pipeline(2)
+    expect(result).toBe(6)
+})
+
+test('pipe passes the results of dbl to inc', () => {
+    const pipeline = pipe(dbl, inc)
+    const result = pipeline(2)
+    expect(result).toBe(5)
+})
+
+test('pipe works with more than 2 functions', () => {
+    const pipeline = pipe(add, inc, dbl, inc)
+    const result = pipeline(1,2)
+    expect(result).toBe(9)
 })
